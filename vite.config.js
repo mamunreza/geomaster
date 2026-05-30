0e33d1c9e92f6e4a0fcabba1a71ec27bd4015333
Mamun Reza <mamun.reza09@gmail.com>
2026-05-31 00:21:03 +0300

    Map library changed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import license from 'rollup-plugin-license'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      ...license({
+        thirdParty: {
+          output: {
+            file: path.join('dist', 'THIRD-PARTY-LICENSES.txt'),
+            template(dependencies) {
+              return dependencies
+                .map(dep =>
+                  [
+                    `${dep.name}@${dep.version}`,
+                    dep.license,
+                    dep.licenseText ?? '(see package for full license text)',
+                  ].join('\n')
+                )
+                .join('\n\n---\n\n');
+            },
+          },
+        },
+      }),
+      apply: 'build',
+    },
+  ],
   build: {
     rollupOptions: {
       output: {
@@ -10,13 +35,13 @@ export default defineConfig({
           vendor: ['react', 'react-dom'],
           router: ['react-router-dom'],
           i18n: ['i18next', 'react-i18next'],
-          maps: ['react-simple-maps'],
-        }
-      }
-    }
+          maps: ['react-simple-maps', 'maplibre-gl'],
+        },
+      },
+    },
   },
   server: {
     port: 3000,
     open: true,
-  }
+  },
 })
